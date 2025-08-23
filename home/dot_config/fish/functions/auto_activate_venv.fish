256e5c1ca050ccf7f8ad7c84639b4cb16796ed79
Steven Kreitzer <skre@skre.me>
2025-08-22 19:03:33 -0500

    add venv

--- home/dot_config/fish/functions/auto_activate_venv.fish
+++ home/dot_config/fish/functions/auto_activate_venv.fish
@@ -1,20 +1,3 @@
-function venv --description "Create and activate a new virtual environment"
-    echo "Creating virtual environment in "(pwd)"/.venv"
-    python3 -m venv .venv --upgrade-deps
-    source .venv/bin/activate.fish
-
-    # Append .venv to the Git exclude file, but only if it's not
-    # already there.
-    if test -e .git
-        set line_to_append ".venv"
-        set target_file ".git/info/exclude"
-
-        if not grep --quiet --fixed-strings --line-regexp "$line_to_append" "$target_file" 2>/dev/null
-            echo "$line_to_append" >> "$target_file"
-        end
-    end
-end
-
 function auto_activate_venv --on-variable PWD --description "Auto activate/deactivate virtualenv when I change directories"
     # Get the top-level directory of the current Git repo (if any)
     set REPO_ROOT (git rev-parse --show-toplevel 2>/dev/null)
